3e47a8783253896c9320a3deb6a4691c6f309dec
MrSourceCoded <sourcecodedcreations@gmail.com>
2014-05-09 20:40:29 +0800

    Basic

--- Minedroid2/src/main/java/sourcecoded/minedroid/Minedroid.java
+++ Minedroid2/src/main/java/sourcecoded/minedroid/Minedroid.java
@@ -1,5 +1,35 @@
 package sourcecoded.minedroid;
 
+import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
+
+@Mod(modid = Minedroid.MODID, version = Minedroid.VERSION)
 public class Minedroid {
 
+	public static final String MODID = "minedroid";
+	public static final String VERSION = "2.0.0";
+	
+	@EventHandler
+    public void preinit(FMLPreInitializationEvent event){
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+    	if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
+    		//FMLCommonHandler.instance().bus().register(TickHandler.instance());    		
+    	}
+    	
+    	
+    	MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) { 
+    }
 }
